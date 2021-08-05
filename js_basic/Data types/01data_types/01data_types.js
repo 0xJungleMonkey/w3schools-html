7dff0b113c55c8e4087ae7f921272e6b32cc7434
Monkey 0xJungle.com <Monkey@0xJungle.com>
2021-08-05 22:29:52 +0800

    5 kinds of data types

---
+++ js_basic/Data types/01data_types/01data_types.js
@@ -0,0 +1,22 @@
+// 检测字面量的数据类型
+console.log(typeof(2));
+console.log(typeof(2.4));
+console.log(typeof(Infinity));
+console.log(typeof(NaN));
+console.log(typeof("字符串"));
+console.log(typeof(undefined));
+console.log(typeof(true));
+console.log(typeof(false));
+console.log(typeof(null));
+console.log(typeof 67);
+console.log(typeof(67 + 78));
+console.log(typeof 67 + 78);
+console.log(typeof("   "))
+
+ // 检测变量的数据类型
+    // 定义变量
+    var a = 1;
+    console.log(typeof(a));
+    // 第二次赋值
+    a = true;
+    console.log(typeof(a));
