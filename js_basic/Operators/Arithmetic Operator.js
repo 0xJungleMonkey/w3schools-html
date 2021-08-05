308673703190011ce380fb7388be298c1ef236b6
Monkey 0xJungle.com <Monkey@0xJungle.com>
2021-08-05 22:54:39 +0800

    Arithmetic Operator Usage

---
+++ js_basic/Operators/Arithmetic Operator.js
@@ -0,0 +1,47 @@
+ // 数字与数字运算
+    // console.log(4 + 5);
+    // console.log(4 * 5);
+    // console.log(4 / 5);
+    // console.log(4 % 5);
+
+    // NaN参与
+    // console.log(NaN + 5);
+    // console.log(NaN * 5);
+    // console.log(NaN / 5);
+    // console.log(NaN % 5);
+
+    // Infinity
+    // console.log(Infinity + 5);
+    // console.log(Infinity - 5);
+    // console.log(Infinity * 5);
+    // console.log(Infinity / 5);
+    // console.log(Infinity % 5);
+    // console.log(-Infinity + 5);
+    // console.log(-Infinity - 5);
+    // console.log(-Infinity * 5);
+    // console.log(-Infinity / 5);
+    // console.log(-Infinity % 5);
+    // console.log(5 / Infinity);
+    // console.log(5 % Infinity);
+    // console.log(Infinity + Infinity);
+    // console.log(Infinity - Infinity);
+    // console.log(Infinity * Infinity);
+    // console.log(Infinity / Infinity);
+    // console.log(Infinity % Infinity);
+
+    // 非正常情况2
+    // 字符串参与
+    console.log(1 + "2");
+    console.log(1 - "2");
+    console.log(1 * "2");
+    console.log(1 / "2");
+    console.log(1 % "2");
+    console.log(1 - "");
+    console.log(1 - "   ");
+    console.log(1 - "12abc");
+
+    // 其他数据类型参与
+    console.log(1 + true);
+    console.log(1 + false);
+    console.log(1 + undefined);
+    console.log(1 + null);
