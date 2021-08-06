65f3de6d092cf3a3cc72bb6c10398a95d9a5dc0f
Monkey 0xJungle.com <Monkey@0xJungle.com>
2021-08-06 23:01:06 +0800

    Assignment operators

---
+++ js_basic/Operators/04assignment_operator.js
@@ -0,0 +1,12 @@
+// 定义变量
+var a = 5;
+// 加等于
+a += 3;  // 等价于  a = a + 3
+console.log(a);
+// 乘等于
+a *= 6; // 等价于  a = a * 6
+console.log(a);
+// 递加
+// console.log(a++);
+console.log(++a);
+console.log(a);
