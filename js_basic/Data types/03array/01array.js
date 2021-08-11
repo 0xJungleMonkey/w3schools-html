5eb075fbe02580a5f3d8759d466440537f7a8c38
Monkey 0xJungle.com <Monkey@0xJungle.com>
2021-08-11 23:47:47 +0800

    Function declaration, call. arguement. data type

--- js_basic/Data types/03array/01array.js
+++ js_basic/Data types/03array/01array.js
@@ -33,17 +33,54 @@
     console.log(arr2);
     arr2.length = 9;
     console.log(arr2);
-
+// 1. 创建数组
     var cars = ["Audi","BMW","porsche"];
     // var array-name = [item1,item2,item3......]
     document.getElementById("demo").innerHTML = cars;
-
+// 2.获取数组element
     var person = {firstName:"Bill",lastName:"Gates", age: 62};
         cars[0] = "Opel";
         cars[3] = person;
     document.getElementById("demo2").innerHTML = "The type of array " + cars + "is "+ typeof cars + "<br>"+ person.firstName;     
-    
+// 4. 获取数组的长度
     var fruits = ["Banana","Orange","Apple","Mango"];
     var last = fruits[fruits.length-1];
     document.getElementById("demo3").innerHTML = fruits.length + "<br>" + last;
-    
+// 6.push() Add element as the last element
+    var fruits = ["Banana","Orange","Apple","Mango"];
+    document.getElementById("demo6").innerHTML = fruits;
+    document.getElementById("demo7").innerHTML = fruits;
+    function myFunction(){
+        fruits.push("Lemon");
+        document.getElementById("demo6").innerHTML = fruits;
+    }
+    function lengthfun(){
+        fruits[fruits.length] = "Lemonn";
+        document.getElementById("demo7").innerHTML = fruits;
+    }
+
+// for loop
+    // 定义一个数组
+    var arr = [45,56,76,88,89,90,100,34,56];
+    // 数组遍历
+    // for (var i = 0 ; i <= arr.length - 1 ;i++) {
+    //   console.log(arr[i]);
+    // }
+
+    // 给数组中每一项数据加 5
+    for (var i = 0; i < arr.length ; i++) {
+    // 获取每一项数组的数据，等号赋新值
+    arr[i] += 5;
+    }
+    console.log(arr);
+// foreach method 
+    var fruits, text;
+    fruits = ["Banana","Orange","Apple","Mango"];
+    text = "<ul>";
+    fruits.foreach(myFunction);
+    text +="</ul>";
+    document.getElementById("demo5").innerHTML = text;
+    function myFunction(value){
+    text += "<li>" + value +"</li>";
+    }
+            
---
+++ js_basic/Data types/04function/01function.js
@@ -0,0 +1,84 @@
+// 1. 1. Function declaration 
+    function myFunctio(p1, p2) {
+        return p1 * p2;
+    }
+    document.getElementById("ne").innerHTML = myFunctio(7, 8);
+
+//1. 2.  Function parameters
+// 定义一个求和函数，传入两个数据
+    // 参数：传两个参数，数据类型为数字
+    // 功能：得到两个数字之和
+    function sum(a,b) {
+      console.log(a + b);
+    }
+    // 调用函数中，给小括号内部添加数据
+    sum(3,4);
+    sum("3",4);
+// 
+    document.getElementById("monkey2").innerHTML=
+    "The tempreature is:" + fToc(88) + "Celsius.";
+    function fToc(fahrenheit){
+        return(5/9)*(fahrenheit-32);
+    }
+
+//1.3 Function return value 
+ // 定义一个求和函数，传入两个数据
+    // 参数：传两个参数，数据类型为数字
+    // 功能：得到两个数字之和
+    // 使用返回值，制作函数运行结束后的结果
+    function sum(a,b) {
+        return a + b;
+      }
+      // 调用函数
+      // console.log(sum(1,2));
+      // 将返回值赋值给变量
+      var num = sum(3,4);
+      console.log(num);
+      // 将返回值赋值给函数的实参
+      console.log(sum(2,sum(3,4)));
+      // return 可以终止函数的执行
+      // function fun() {
+      //   console.log(1);
+      //   console.log(2);
+      //   console.log(3);
+      //   return;
+      //   console.log(4);
+      //   console.log(5);
+      //   console.log(6);
+      // }
+      // // 函数调用
+      // fun();
+// 1.5 Function Expression
+ // 定义一个函数表达式
+ var foo = function fun() {
+    console.log(1);
+  };
+  var foo2 = function () {
+    console.log(2);
+  };
+  // 调用函数式，只能用变量名调用，函数名调用不成功
+  // fun();
+  foo();
+  foo2();
+//Case 2 
+// 定义一个函数表达式
+var X =yourFunction(8,8);
+// 调用函数式，只能用变量名调用，函数名调用不成功
+document.getElementById("monkey1").innerHTML= X;
+// define the function 
+function yourFunction(c,d){
+    return c + d;
+}
+
+//4.  
+(function () {
+    document.getElementById("demo1").innerHTML = "Hello! I called myself";
+  })();
+
+// 
+function myFunction(a, b) {
+    return arguments.length;
+}
+document.getElementById("demo2").innerHTML = myFunction(4, 3);
+
+
